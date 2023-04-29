9748ec0c2bc9471dcb375c504c810063a4f70c35
anud12 <supstanta_andu@yahoo.com>
2023-04-29 16:06:31 +0000

    commit

---
+++ service/target/debug/build/num-traits-eddf136086a8cb8a/build_script_build-eddf136086a8cb8a.d
@@ -0,0 +1,5 @@
+/workspaces/anud12.github.io_boxes/service/target/debug/build/num-traits-eddf136086a8cb8a/build_script_build-eddf136086a8cb8a: /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/num-traits-0.2.15/build.rs
+
+/workspaces/anud12.github.io_boxes/service/target/debug/build/num-traits-eddf136086a8cb8a/build_script_build-eddf136086a8cb8a.d: /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/num-traits-0.2.15/build.rs
+
+/usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/num-traits-0.2.15/build.rs:
---
+++ service/target/debug/build/serde_derive-053ce0aa784533a7/build_script_build-053ce0aa784533a7.d
@@ -0,0 +1,5 @@
+/workspaces/anud12.github.io_boxes/service/target/debug/build/serde_derive-053ce0aa784533a7/build_script_build-053ce0aa784533a7: /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/serde_derive-1.0.160/build.rs
+
+/workspaces/anud12.github.io_boxes/service/target/debug/build/serde_derive-053ce0aa784533a7/build_script_build-053ce0aa784533a7.d: /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/serde_derive-1.0.160/build.rs
+
+/usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/serde_derive-1.0.160/build.rs:
