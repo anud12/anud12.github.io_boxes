a4b8bd2b5a3476d11549a1b6d2a78547c652c3e2
anud12 <supstanta_andu@yahoo.com>
2023-04-29 16:06:52 +0000

    commit

--- service/target/debug/build/serde_json-57a1af60c8c5b061/build_script_build-57a1af60c8c5b061.d
+++
@@ -1,5 +0,0 @@
-/workspaces/anud12.github.io_boxes/service/target/debug/build/serde_json-57a1af60c8c5b061/build_script_build-57a1af60c8c5b061: /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/serde_json-1.0.96/build.rs
-
-/workspaces/anud12.github.io_boxes/service/target/debug/build/serde_json-57a1af60c8c5b061/build_script_build-57a1af60c8c5b061.d: /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/serde_json-1.0.96/build.rs
-
-/usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/serde_json-1.0.96/build.rs:
---
+++ service/target/debug/deps/spin-a9be327916733dcf.d
@@ -0,0 +1,8 @@
+/workspaces/anud12.github.io_boxes/service/target/debug/deps/spin-a9be327916733dcf.rmeta: /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/spin-0.5.2/src/lib.rs /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/spin-0.5.2/src/mutex.rs /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/spin-0.5.2/src/rw_lock.rs /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/spin-0.5.2/src/once.rs
+
+/workspaces/anud12.github.io_boxes/service/target/debug/deps/spin-a9be327916733dcf.d: /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/spin-0.5.2/src/lib.rs /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/spin-0.5.2/src/mutex.rs /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/spin-0.5.2/src/rw_lock.rs /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/spin-0.5.2/src/once.rs
+
+/usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/spin-0.5.2/src/lib.rs:
+/usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/spin-0.5.2/src/mutex.rs:
+/usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/spin-0.5.2/src/rw_lock.rs:
+/usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/spin-0.5.2/src/once.rs:
