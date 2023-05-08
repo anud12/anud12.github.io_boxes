d8c2082d7de23c0eb296f50b1b3bc2994f025ef1
alexandru.crihan <alexandru.crihan@esolutions.ro>
2023-05-08 13:47:00 +0300

    commit

--- service/src/main.rs
+++ service/src/main.rs
@@ -8,11 +8,12 @@ mod implementation;
 mod printable;
 fn main() -> Result<(), Box<dyn Error>> {
     // Load the service account JSON file
-    let sa_file = include_str!("service_account.json");
-    let sa_json: serde_json::Value = serde_json::from_str(&sa_file).unwrap();
-    let private_key = sa_json["private_key"].as_str().unwrap();
-    let client_email = sa_json["client_email"].as_str().unwrap();
-
+    // let sa_file = include_str!("service_account.json");
+    // let sa_json: serde_json::Value = serde_json::from_str(&sa_file).unwrap();
+    // let private_key = sa_json["private_key"].as_str().unwrap();
+    // let client_email = sa_json["client_email"].as_str().unwrap();
+    let private_key = "";
+    let client_email = "";
     let global_fs = GoogleSession::new(client_email, private_key)?.drive();
     let boxes = global_fs.find_one_by_name("boxes")?;
     let boxes_trash = global_fs.find_one_by_name("boxes_trash")?;
