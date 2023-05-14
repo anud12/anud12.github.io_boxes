01105207ec4879b3dbd6a41269de9c6c8d8ee555
anud12 <supstanta_andu@yahoo.com>
2023-05-14 11:05:59 +0000

    commit

--- service/src/main.rs
+++ service/src/main.rs
@@ -1,19 +1,11 @@
+use job_base::PostPrintable;
+pub use job_base::{FileMetadata, FolderQuery, GoogleSession, RootQuery};
 use std::error::Error;
-pub use job_base::{GoogleSession, RootQuery, FolderQuery, FileMetadata};
 fn main() -> Result<(), Box<dyn Error>> {
-    // Load the service account JSON file
-    // let sa_file = include_str!("service_account.json");
-    // let sa_json: serde_json::Value = serde_json::from_str(&sa_file).unwrap();
-    // let private_key = sa_json["private_key"].as_str().unwrap();
-    // let client_email = sa_json["client_email"].as_str().unwrap();
-    let private_key = "";
-    let client_email = "";
-    let global_fs = GoogleSession::new(client_email, private_key)?.drive();
+    let global_fs = GoogleSession::new()?.into_drive();
     let boxes = global_fs.find_one_by_name("boxes")?;
+    boxes.find_all().print("All files in Boxes:");
     let boxes_trash = global_fs.find_one_by_name("boxes_trash")?;
     let first = boxes.find_by_name("first.json")?.remove(0);
-    //
-    // first.get_body_json().print();
-    // global_fs.find_all()?.print();
     Ok(())
 }
