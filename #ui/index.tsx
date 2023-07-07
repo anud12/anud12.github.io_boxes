d2064e98745f167db319a2b51b5d38b530b1fe43
alexandru.crihan <alexandru.crihan@esolutions.ro>
2023-07-07 16:54:36 +0300

    commit

--- #ui/index.tsx
+++ #ui/index.tsx
@@ -5,6 +5,6 @@ import { Link } from "anud12.github.io_ui_base/src/components/atoms/Link";
 module.exports = (
     <Page title="Boxes">
         <Link href={"add"}>Add</Link>
-        <Table source="1DwTbUSWf5kzNq84Kc08bJ9Wyw9ijfBuS" />
+        <Table source="1BlHXnJJPuWu0_VDe-JAJzZ0XAR_MplEW45TnmdMeKfI" />
     </Page >
 )
