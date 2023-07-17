380fd2b192e4805be07fb542f7c7b66aed15064c
alexandru.crihan <alexandru.crihan@esolutions.ro>
2023-07-17 14:32:37 +0300

    commit

--- #/job.js
+++ #/job.js
@@ -1,16 +1,16 @@
 const newGoogleSession = require("anud12.github.io_job_base");
 module.exports = (async () => {
     const globalFs = (await newGoogleSession()).intoDrive();
-    const create = await globalFs.findOneById("11zqF7BKA-7hvcONuSmbU2RGHrxmLafNl");
-    const fileList = await create.findByName("form-data.json");
+    const pending = await globalFs.findOneByIdLazy("11zqF7BKA-7hvcONuSmbU2RGHrxmLafNl");
+    const fileList = await pending.findByName("form-data.json");
     if(!fileList?.length) {
         return;
     }
     const trash = await globalFs.findOneById("1NwJuSyQ4rFoI6I07vAwjmm62X3zuCNxv");
     const db = (await globalFs.findOneById("1BlHXnJJPuWu0_VDe-JAJzZ0XAR_MplEW45TnmdMeKfI")).intoSheet()
     const images = await globalFs.findOneById("1gfpLitPAZtWF5omk97dB3IXbf_leWXPp");
-    
-    
+
+
     const rows = await Promise.all(fileList.map(async file => {
         const data = await file.bodyJson();
         const imageData = new Buffer(data.image.data, 'base64');
