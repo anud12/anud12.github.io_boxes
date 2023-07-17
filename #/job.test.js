ee44d05a8d8ca3a995ecbf1df742f18acfb46592
alexandru.crihan <alexandru.crihan@esolutions.ro>
2023-07-17 14:33:23 +0300

    commit

--- #/job.test.js
+++ #/job.test.js
@@ -3,5 +3,5 @@ const readFileSync = require("fs").readFileSync;
 it("runs", async () => {
     process.env.CLIENT_EMAIL = readFileSync(`${__dirname}/client_email`).toString();
     process.env.PRIVATE_KEY = readFileSync(`${__dirname}/private_key`).toString();
-    await require("./index");
+    await require("./job");
 })
