c4dd6200ff128435f95ba74de674af6fb4280976
anud12 <supstanta_andu@yahoo.com>
2023-07-23 01:32:36 +0000

    commit

--- #/build.js
+++ #/build.js
@@ -1,5 +1,7 @@
 const config = {
-    external: ["anud12.github.io_job_base"]
+    external: {
+        "anud12.github.io_job_base": "{}"
+    }
 }
 require("anud12.github.io_ui_compiler").JSX(`${__dirname}/index.tsx`, `${__dirname}/../index`, config);
 require("anud12.github.io_ui_compiler").JSX(`${__dirname}/index_add.tsx`, `${__dirname}/../add/index`, config);
